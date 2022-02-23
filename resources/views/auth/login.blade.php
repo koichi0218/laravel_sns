86427714311c73041bba604066cc689f64eb8ab2
koichi0218 <kouiti0218@gmail.com>
2022-02-23 22:06:35 +0900

    ログイン、新規登録レイアウト

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,24 +1,18 @@
 @extends('layouts.not_logged_in')
  
 @section('content')
-  <h1>ログイン</h1>
- 
-  <form method="POST" action="{{ route('login') }}">
-      @csrf
-      <div>
-          <label>
-            メールアドレス:
-            <input type="email" name="email">
-          </label>
-      </div>
- 
-      <div>
-          <label>
-            パスワード:
-            <input type="password" name="password" >
-          </label>
-      </div>
- 
-      <input type="submit" value="ログイン">
-  </form>
+ <div class = "container">
+  	<div class= "wrapper">
+  	  <form method="POST" action="{{route('login')}}" name="Login_Form" class="form-signin">
+  	    @csrf     
+  		    <h3 class="form-signin-heading">サインイン</h3>
+  			  <hr class="colorgraph"><br>
+  			  
+  			  <input type="text" class="form-control" name="email" placeholder="email">
+  			  <input type="password" class="form-control" name="password" placeholder="password">     		  
+  			 
+  			  <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">ログイン</button>  			
+  		</form>			
+  	</div>
+ </div>
 @endsection
--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -2,8 +2,13 @@
 <html lang="ja">
 <head>
     <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>@yield('title')</title>
     <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+    <link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+    <script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <body>
     @yield('header')
