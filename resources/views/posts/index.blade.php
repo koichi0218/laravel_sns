0c11dde3c8a2fd60d6a6d9c11800a0001702907b
koichi0218 <kouiti0218@gmail.com>
2022-02-27 23:00:06 +0900

    ユーザー情報

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -10,16 +10,26 @@
           <div class="row">
             <div class="col-md-12">
               <div class="card mb-4 shadow-sm">
-                  //ここに画像をいれる
-                <img class="card-img-top" data-src="holder.js/100px225?theme=thumb&bg=55595c&fg=eceeef&text=Thumbnail" alt="Card image cap">
+                <div>{{$post->user->name}}</div>
+                @if($post->image !== '')
+                  <img class="card-img-top" src="{{ \Storage::url($post->image) }}" alt="Card image cap">
+                @else
+                  <img src="{{ asset('images/no_image.png') }}">
+                @endif
                 <div class="card-body">
                   <p class="card-text">{{$post->comment}}</p>
                   <div class="d-flex justify-content-between align-items-center">
                     <div class="btn-group">
                       <button type="button" class="btn btn-sm btn-outline-secondary">View</button>
-                      <button type="button" class="btn btn-sm btn-outline-secondary">Edit</button>
+                      <button type="button" class="btn btn-sm btn-outline-secondary" onClick=location.href="{{ route('posts.edit', $post)}}">編集</button>
                     </div>
-                    <small class="text-muted">9 mins</small>
+                    <a href="{{ route('posts.edit_image', $post) }}">画像を変更</a>
+                    <form method="post" class="delete" action="{{ route('posts.destroy', $post) }}">
+                      @csrf
+                      @method('delete')
+                      <input type="submit" value="削除">
+                    </form>
+                    <small class="text-muted">{{$post->created_at}}</small>
                   </div>
                 </div>
               </div>
---
+++ resources/views/users/show.blade.php
@@ -0,0 +1,21 @@
+@extends('layouts.logged_in')
+ 
+@section('title', $title)
+ 
+@section('content')
+<div class="container">
+    <div class="card mb-3">
+      <svg class="bd-placeholder-img card-img-top" width="100%" height="180" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Image cap"><title>Placeholder</title><rect width="100%" height="100%" fill="#868e96"/><text x="50%" y="50%" fill="#dee2e6" dy=".3em">Image cap</text></svg>
+      <div class="card-body">
+        <h5 class="card-title">Card title</h5>
+        <p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+        <p class="card-text"><small class="text-muted">Last updated 3 mins ago</small></p>
+      </div>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">フォロー数</li>
+        <li class="list-group-item">フォロワー数</li>
+        <li class="list-group-item">いいね数</li>
+      </ul>
+    </div>
+</div>
+@endsection
