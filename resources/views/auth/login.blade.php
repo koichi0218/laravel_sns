c7174747f3ac46ee665c0d4e4cbc5bb2341067ff
student_51445 <kouiti0218@gmail.com>
2022-03-24 10:49:28 +0000

    デプロイ用修正

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,21 @@
 @extends('layouts.not_logged_in')
  
 @section('content')
-<body class="login-img">
-
-  <div class = "container login_container">
-   	<div class= "wrapper">
-   	  <form method="POST" action="{{route('login')}}" name="Login_Form" class="form-signin">
-   	    @csrf     
-   		    <h3 class="form-signin-heading">サインイン</h3>
-   			  <hr class="colorgraph"><br>
-   			  <input type="text" class="form-control" name="email" placeholder="email">
-   			  <input type="password" class="form-control" name="password" placeholder="password">     		  
-   			  <button class="btn btn-lg btn-primary btn-block"  name="Submit" value="Login" type="Submit">ログイン</button>  			
-   		</form>
-   	</div>
+<div class = "container login_container">
+  <div class= "wrapper">
+    <form method="POST" action="{{route('login')}}" name="Login_Form" class="form-signin">
+      @csrf
+      <h3 class="form-signin-heading">サインイン</h3>
+      <hr class="colorgraph"><br>
+      <input type="text" class="form-control" name="email" placeholder="email">
+      <input type="password" class="form-control" name="password" placeholder="password">
+      <button class="btn btn-lg btn-primary btn-block" name="Submit" value="Login" type="Submit">ログイン</button>
+    </form>
   </div>
-</body>
+</div>
+<script>
+window.onload = function() {
+document.body.classList.add('login-img');
+}
+</script>
 @endsection
